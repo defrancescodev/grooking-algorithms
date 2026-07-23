ef73f2a83f20afb61b1b221b8f545d0b65655f50
Samuel De Francesco <defrancescodev@gmail.com>
2026-07-23 15:08:57 -0300

    played a little with dictionary

---
+++ grooking-algorithms/chapter05/main.py
@@ -0,0 +1,21 @@
+hash_map = dict()
+hash_map2 = {"Maça": 1.57, "Apple": 2.46 }
+hash_map["Maça"] = 1.49
+agenda_telefonica = {}
+agenda_telefonica["Samuel"] = 992600678
+agenda_telefonica["Polícia"] = 190
+
+
+print(hash_map["Maça"])
+print(hash_map2["Apple"])
+print(agenda_telefonica["Polícia"])
+
+people_who_voted = {}
+
+#def verify_elector(name):
+#    if people_who_voted.get(name):
+#        print("Manda embora!")
+#    else:
+#        people_who_voted[name] = True
+#        print("Deixe votar!")
+
